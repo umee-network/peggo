c3ba2419dd5fe29091fba3ebf901af07dc4fd328
Facundo Medica <facundomedica@gmail.com>
2021-11-01 18:09:45 +0100

    cleanup

--- test/run-gh-tests.sh
+++ test/run-gh-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 npm install -g ganache-cli
+ganache-cli -h "0.0.0.0" -p 8545 -m "concert load couple harbor equip island argue ramp clarify fence smart topic" -l 999999999999999 &
 
 wget https://binaries.soliditylang.org/linux-amd64/solc-linux-amd64-v0.8.2+commit.661d1103
 mv solc-linux-amd64-v0.8.2+commit.661d1103 solc && chmod +x solc && mv solc /usr/local/bin/solc
@@ -12,5 +13,4 @@ make install
 cd ..
 
 CHAIN_ID=umee-local STAKE_DENOM=uumee DENOM=uumee CLEANUP=1 ./test/cosmos/multinode.sh umeed
-ganache-cli -h "0.0.0.0" -p 8545 -m "concert load couple harbor equip island argue ramp clarify fence smart topic" -l 999999999999999 &
 PEGGO_TEST_EVM_RPC="http://0.0.0.0:8545" go test ./...
