ba3eeef64828afa2315a476b27bff1599298f761
Facundo Medica <facundomedica@gmail.com>
2021-12-22 15:25:48 -0300

    fix mock

--- mocks/peggy/peggy_contract.go
+++ mocks/peggy/peggy_contract.go
@@ -232,18 +232,18 @@ func (mr *MockContractMockRecorder) Provider() *gomock.Call {
 }
 
 // SendTx mocks base method.
-func (m *MockContract) SendTx(arg0 context.Context, arg1 common.Address, arg2 []byte) (common.Hash, error) {
+func (m *MockContract) SendTx(arg0 context.Context, arg1 common.Address, arg2 []byte, arg3 uint64, arg4 *big.Int) (common.Hash, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "SendTx", arg0, arg1, arg2)
+	ret := m.ctrl.Call(m, "SendTx", arg0, arg1, arg2, arg3, arg4)
 	ret0, _ := ret[0].(common.Hash)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
 // SendTx indicates an expected call of SendTx.
-func (mr *MockContractMockRecorder) SendTx(arg0, arg1, arg2 interface{}) *gomock.Call {
+func (mr *MockContractMockRecorder) SendTx(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendTx", reflect.TypeOf((*MockContract)(nil).SendTx), arg0, arg1, arg2)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendTx", reflect.TypeOf((*MockContract)(nil).SendTx), arg0, arg1, arg2, arg3, arg4)
 }
 
 // SubscribeToPendingTxs mocks base method.
