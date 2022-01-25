dbd05977e9a762a18ae8a654f509232955de1832
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2022-01-25 10:00:15 -0500

    Update cmd/peggo/bridge.go

--- cmd/peggo/bridge.go
+++ cmd/peggo/bridge.go
@@ -345,6 +345,7 @@ network starting.`,
 			denomBase := args[1]
 			denomName := args[2]
 			denomSymbol := args[3]
+
 			denomDecimals, err := strconv.ParseUint(args[4], 10, 8)
 			if err != nil {
 				return fmt.Errorf("invalid denom decimals: %w", err)
