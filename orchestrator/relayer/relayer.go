1bb5c3faa8270bfae14870565d5f2fc5facce849
Facundo Medica <14063057+facundomedica@users.noreply.github.com>
2022-02-20 20:40:34 +0100

    Update orchestrator/relayer/relayer.go
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- orchestrator/relayer/relayer.go
+++ orchestrator/relayer/relayer.go
@@ -13,14 +13,15 @@ import (
 	gravitytypes "github.com/Gravity-Bridge/Gravity-Bridge/module/x/gravity/types"
 )
 
+// ValsetRelayMode defines an enumerated validator set relay mode.
 type ValsetRelayMode int64
 
+// Allowed validator set relay modes
 const (
 	ValsetRelayModeNone ValsetRelayMode = iota
 	ValsetRelayModeMinimum
 	ValsetRelayModeAll
 )
-
 type GravityRelayer interface {
 	Start(ctx context.Context) error
 
