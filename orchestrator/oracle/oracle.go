386eecffc5f31bc09aafd92ace21a7e0bbdf799a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-03-16 19:09:46 -0300

    add interface to oracle

--- orchestrator/oracle/oracle.go
+++ orchestrator/oracle/oracle.go
@@ -21,6 +21,7 @@ import (
 const (
 	tickerTimeout        = 1000 * time.Millisecond
 	availablePairsReload = 24 * time.Hour
+	BaseSymbolETH        = "ETH"
 )
 
 var (
@@ -29,15 +30,24 @@ var (
 	deviationThreshold = sdk.MustNewDecFromStr("2")
 )
 
+// PriceFeeder defines an interface an oracle with N exchange price
+// provider must implement.
+type PriceFeeder interface {
+	GetPrices(baseSymbols ...string) (map[string]sdk.Dec, error)
+	GetPrice(baseSymbol string) (sdk.Dec, error)
+	SubscribeSymbols(baseSymbols ...string) error
+}
+
 // Oracle implements the core component responsible for fetching exchange rates
 // for a given set of currency pairs and determining the correct exchange rates.
 type Oracle struct {
 	logger zerolog.Logger
 	closer *ummedpfsync.Closer
 
-	mtx       sync.RWMutex
-	providers map[string]*Provider // providerName => Provider
-	prices    map[string]sdk.Dec   // symbol => price
+	mtx                   sync.RWMutex
+	providers             map[string]*Provider // providerName => Provider
+	prices                map[string]sdk.Dec   // baseSymbol => price ex.: UMEE, ETH => sdk.Dec
+	subscribedBaseSymbols map[string]struct{}  // baseSymbol => nothing
 }
 
 // Provider wraps the umee provider interface.
@@ -64,48 +74,73 @@ func New(ctx context.Context, logger zerolog.Logger, providersName []string) (*O
 	}
 
 	oracle := &Oracle{
-		logger:    logger.With().Str("module", "oracle").Logger(),
-		closer:    ummedpfsync.NewCloser(),
-		providers: providers,
+		logger:                logger.With().Str("module", "oracle").Logger(),
+		closer:                ummedpfsync.NewCloser(),
+		providers:             providers,
+		subscribedBaseSymbols: map[string]struct{}{},
 	}
-	oracle.LoadAvailablePairs()
+	oracle.loadAvailablePairs()
+	go oracle.start(ctx)
 
 	return oracle, nil
 }
 
-// GetPrices returns a copy of the current prices fetched from the oracle's
-// set of exchange rate providers.
-func (o *Oracle) GetPrices(symbols ...string) map[string]sdk.Dec {
+// GetPrices returns the price for the provided base symbols.
+func (o *Oracle) GetPrices(baseSymbols ...string) (map[string]sdk.Dec, error) {
 	o.mtx.RLock()
 	defer o.mtx.RUnlock()
 
 	// Creates a new array for the prices in the oracle
-	prices := make(map[string]sdk.Dec, len(o.prices))
-	for k, v := range o.prices {
-		// Fills in the prices with each value in the oracle
-		prices[k] = v
+	prices := make(map[string]sdk.Dec, len(baseSymbols))
+
+	for _, baseSymbol := range baseSymbols {
+		price, ok := o.prices[baseSymbol]
+		if !ok {
+			return nil, fmt.Errorf("error getting price for %s", baseSymbol)
+		}
+		prices[baseSymbol] = price
 	}
 
-	return prices
+	return prices, nil
+}
+
+// GetPrice returns the price based on the base symbol ex.: UMEE, ETH.
+func (o *Oracle) GetPrice(baseSymbol string) (sdk.Dec, error) {
+	o.mtx.RLock()
+	defer o.mtx.RUnlock()
+
+	price, ok := o.prices[baseSymbol]
+	if !ok {
+		return sdk.Dec{}, fmt.Errorf("error getting price for %s", baseSymbol)
+	}
+
+	return price, nil
 }
 
 // SubscribeSymbols attempts to subscribe the symbols in all the providers.
 // baseSymbols is the base to be subscribed ex.: ["UMEE", "ATOM"].
 func (o *Oracle) SubscribeSymbols(baseSymbols ...string) error {
+	o.mtx.Lock()
+	defer o.mtx.Unlock()
+
 	for _, baseSymbol := range baseSymbols {
+		_, ok := o.subscribedBaseSymbols[baseSymbol]
+		if ok {
+			// pair already subscribed
+			continue
+		}
+
 		currencyPairs := GetStablecoinsCurrencyPair(baseSymbol)
 		if err := o.subscribeProviders(currencyPairs); err != nil {
 			return err
 		}
+		o.subscribedBaseSymbols[baseSymbol] = struct{}{}
 	}
 
 	return nil
 }
 
 func (o *Oracle) subscribeProviders(currencyPairs []umeedpftypes.CurrencyPair) error {
-	o.mtx.Lock()
-	defer o.mtx.Unlock()
-
 	for providerName, provider := range o.providers {
 		var pairsToSubscribe []umeedpftypes.CurrencyPair
 
@@ -118,9 +153,9 @@ func (o *Oracle) subscribeProviders(currencyPairs []umeedpftypes.CurrencyPair) e
 				continue
 			}
 
-			_, ok = provider.availablePairs[symbol]
-			if !ok {
-				o.logger.Debug().Str("provider name", providerName).Str("symbol", symbol).Msg("symbol is not available")
+			_, availablePair := provider.availablePairs[symbol]
+			if !availablePair {
+				o.logger.Debug().Str("provider_name", providerName).Str("symbol", symbol).Msg("symbol is not available")
 				continue
 			}
 
@@ -128,9 +163,11 @@ func (o *Oracle) subscribeProviders(currencyPairs []umeedpftypes.CurrencyPair) e
 		}
 
 		if err := provider.SubscribeCurrencyPairs(pairsToSubscribe...); err != nil {
-			o.logger.Err(err).Str("provider name", providerName).Msg("subscribing to new currency pairs")
+			o.logger.Err(err).Str("provider_name", providerName).Msg("subscribing to new currency pairs")
 			return err
 		}
+
+		o.logger.Info().Msgf("Subscribed pairs %+v in provider: %s", pairsToSubscribe, providerName)
 
 		for _, pair := range pairsToSubscribe {
 			provider.subscribedPairs[pair.String()] = pair
@@ -146,32 +183,30 @@ func (o *Oracle) Stop() {
 	<-o.closer.Done()
 }
 
-// Start starts the oracle process in a blocking fashion.
-func (o *Oracle) Start(ctx context.Context) error {
+// start starts the oracle process in a blocking fashion.
+func (o *Oracle) start(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
 			o.closer.Close()
 
 		case <-time.After(tickerTimeout):
-			o.logger.Debug().Msg("starting oracle tick")
-
-			if err := o.tick(ctx); err != nil {
+			if err := o.tick(); err != nil {
 				o.logger.Err(err).Msg("oracle tick failed")
 			}
 
 		case <-time.After(availablePairsReload):
-			o.LoadAvailablePairs()
+			o.loadAvailablePairs()
 		}
 	}
 }
 
-// LoadAvailablePairs load all the available pairs from providers.
-func (o *Oracle) LoadAvailablePairs() {
+// loadAvailablePairs load all the available pairs from providers.
+func (o *Oracle) loadAvailablePairs() {
 	for providerName, provider := range o.providers {
 		availablePairs, err := provider.GetAvailablePairs()
 		if err != nil {
-			o.logger.Debug().Err(err).Str("provider name", providerName).Msg("Error getting available pairs for provider")
+			o.logger.Debug().Err(err).Str("provider_name", providerName).Msg("Error getting available pairs for provider")
 			continue
 		}
 		if len(availablePairs) == 0 {
@@ -181,12 +216,12 @@ func (o *Oracle) LoadAvailablePairs() {
 	}
 }
 
-// SetPrices retrieves all the prices and candles from our set of providers as
+// setPrices retrieves all the prices and candles from our set of providers as
 // determined in the config. If candles are available, uses TVWAP in order
 // to determine prices. If candles are not available, uses the most recent prices
 // with VWAP. Warns the the user of any missing prices, and filters out any faulty
 // providers which do not report prices or candles within 2𝜎 of the others.
-func (o *Oracle) SetPrices(ctx context.Context) error {
+func (o *Oracle) setPrices() error {
 	g := new(errgroup.Group)
 	mtx := new(sync.Mutex)
 	providerPrices := make(umeedpfprovider.AggregatedProviderPrices)
@@ -228,11 +263,6 @@ func (o *Oracle) SetPrices(ctx context.Context) error {
 				cp, candlesOk := candles[pair.String()]
 				if candlesOk {
 					providerCandles[providerName][pair.Base] = cp
-				}
-
-				if !pricesOk && !candlesOk {
-					mtx.Unlock()
-					return fmt.Errorf("failed to find any exchange rates in provider responses")
 				}
 			}
 
@@ -409,14 +439,11 @@ func (o *Oracle) filterTickerDeviations(
 	return filteredPrices, nil
 }
 
-func (o *Oracle) tick(ctx context.Context) error {
-	o.logger.Debug().Msg("executing oracle tick")
-
-	if err := o.SetPrices(ctx); err != nil {
+func (o *Oracle) tick() error {
+	if err := o.setPrices(); err != nil {
 		return err
 	}
 
-	o.logger.Debug().Msg("finish oracle tick")
 	return nil
 }
 
